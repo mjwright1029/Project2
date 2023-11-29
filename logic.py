0d10324f2bc81febc7f52f86a05807b5bb8b585d
mjwright1029 <151294605+mjwright1029@users.noreply.github.com>
2023-11-28 21:07:02 -0600

    Changed GUI and wrote the basic functions

--- logic.py
+++ logic.py
@@ -9,17 +9,18 @@ class Logic(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
-        self.pushButton.clicked.connect(lambda: self.main())
+        self.villager_image.setText('Learn more about the canine villagers \nof Animal Crossing: New Horizons!')
+        self.name_label.setText('Name:')
+        self.gender_label.setText('Gender: ')
+        self.personality_label.setText('Personality: ')
+        self.hobby_label.setText('Hobby: ')
+        self.birthday_label.setText('Birthday: ')
+        self.name_menue.addItems(['Bea', 'Benjamin', 'Biskit', 'Bones', 'Butch', 'Cherry', 'Cookie', 'Daisy',
+                                  'Goldie', 'Lucky', 'Mac', 'Maddie', 'Marcel', 'Portia', 'Shep', 'Walker'])
+        self.name_menue.currentTextChanged.connect(lambda: self.main())
 
     def get_info(self):
-        villager_name = self.lineEdit.text()
-        image = ''
-        URL = ''
-        species = ''
-        gender = ''
-        personality = ''
-        hobby = ''
-        birthday = ''
+        villager_name = self.name_menue.currentText()
         with open('villagers.csv', 'r', newline='', encoding='utf-8') as csvfile:
             content = csv.reader(csvfile, delimiter=',')
 
@@ -31,13 +32,20 @@ class Logic(QMainWindow, Ui_MainWindow):
                     personality = row[6]
                     hobby = row[8]
                     birthday = row[9]
-                    return row, image, species, gender, personality, hobby, birthday
+                    return villager_name, image, species, gender, personality, hobby, birthday
 
     def display_image(self, image):
         pixmap = QPixmap(image)
-        self.label.setPixmap(pixmap)
+        self.villager_image.setPixmap(pixmap)
+
+    def display_info(self, name, gender, personality, hobby, birthday):
+        self.name_label.setText(f'Name: {name}')
+        self.gender_label.setText(f'Gender: {gender}')
+        self.personality_label.setText(f'Personality: {personality}')
+        self.hobby_label.setText(f'Hobby: {hobby}')
+        self.birthday_label.setText(f'Birthday: {birthday}')
 
     def main(self):
-        info, image, species, gender, personality, hobby, birthday = self.get_info()
-        print(f'Species: {species}, Gender: {gender}, Personality: {personality}, Hobby: {hobby}, Birthday: {birthday}')
+        name, image, species, gender, personality, hobby, birthday = self.get_info()
         self.display_image(image)
+        self.display_info(name, gender, personality, hobby, birthday)
